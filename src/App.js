cf247fb3f54aa2855092605e8b7c603a66001376
Parfait kouassi <parfaitkouess@gmail.com>
2021-02-15 14:40:23 +0000

    maternidad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import SignIn from './components/login/login';
+import Maternidad from './components/maternidad/maternidad';
+import Home from './components/home/home';
+import Graphic from './components/graph/graph';
+import Document from './components/document/document';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<Switch>
+  <Route exact path='/' component={SignIn}></Route>
+  <Route exact path='/home' component={Home}></Route>
+  <Route exact path='/maternidad' component={Maternidad}></Route>
+  <Route exact path='/graphic' component={Graphic}></Route>
+  <Route exact path='/document' component={Document}></Route>
+</Switch>
+ )
 }
 
 export default App;
